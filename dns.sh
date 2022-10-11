f967f61b915192d8c8c2ebc1ea353e6432bf0b06
Taylor Monacelli <taylormonacelli@gmail.com>
2022-10-10 22:44:06 -0700

    real men dont make git commit comments

---
+++ dns.sh
@@ -0,0 +1,56 @@
+#! /usr/bin/env bash
+set -euo pipefail
+IFS=$'\t\n'
+
+# https://discuss.linuxcontainers.org/t/whats-the-deal-with-etc-default-lxd-bridge-upgraded/330/3
+
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update --yes --quiet
+apt-get upgrade --yes --quiet
+
+# Upgrade and setup LXD
+
+apt-get install --yes --quiet --target-release=xenial-backports lxd
+
+lxd init --auto
+lxc network create lxdbr0 \
+  ipv4.address=auto \
+  ipv4.nat=true \
+  ipv6.address=auto \
+  ipv6.nat=true
+lxc network attach-profile lxdbr0 default
+
+# Install and setup dnsmasq
+apt-get install --yes --quiet dnsmasq
+
+lxd_bridge="lxdbr0"
+lxd_ipv4_addr="$(
+  lxc network get "${lxd_bridge}" ipv4.address |
+  cut \
+    --delimiter='/' \
+    --fields=1
+)"
+# Create dnsmasq configuration from template
+echo "
+# Tell any system-wide dnsmasq instance to make sure to bind to interfaces
+# instead of listening on 0.0.0.0
+# WARNING: changes to this file will get lost if lxd is removed.
+server=/lxd/${lxd_ipv4_addr}
+bind-interfaces
+except-interface=${lxd_bridge}
+" > /etc/dnsmasq.d/lxd
+
+systemctl restart dnsmasq.service
+After that I can do things like:
+
+ubuntu@ubuntu-xenial:~$ lxc launch images:alpine/3.6 ca
+Creating ca
+Starting ca                                 
+ubuntu@ubuntu-xenial:~$ lxc launch images:alpine/3.6 cb
+Creating cb
+Starting cb
+ubuntu@ubuntu-xenial:~$ lxc launch images:alpine/3.6 cc
+Creating cc
+Starting cc
+ubuntu@u

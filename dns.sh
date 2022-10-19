a9871e6f8ed1dca57ed0bfd903970e4d0987a358
Taylor Monacelli <taylormonacelli@gmail.com>
2022-10-19 16:25:12 -0700

    real men dont make git commit comments

--- dns.sh
+++ dns.sh
@@ -17,6 +17,7 @@ apt-get update --yes --quiet
 apt-get install --yes --quiet lxd
 
 lxd init --auto
+lxc network show lxdbr0
 lxc network delete --force lxdbr0
 lxc network create lxdbr0 \
   ipv4.address=auto \

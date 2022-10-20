58ffc07b87eac67dc0ce932e92e4430960beff44
Taylor Monacelli <taylormonacelli@gmail.com>
2022-10-19 18:44:27 -0700

    real men dont make git commit comments

--- test2.sh
+++ test2.sh
@@ -5,4 +5,7 @@ cd docker-ubuntu-systemd
 
 docker run --name myubuntu --rm --tty --privileged --volume /sys/fs/cgroup:/sys/fs/cgroup:ro taylorm/ubuntu -d
 sleep 5
+
+docker exec myubuntu -- bash -c 'apt -qq install'
+docker exec myubuntu -- bash -c 'apt -qy install iputils-ping'
 docker exec myubuntu -- bash -c 'ping -c 10 google.com'

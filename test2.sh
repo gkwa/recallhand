f6a3c3d8d392f8d70a0675cf5e4ad06c421ea99f
Taylor Monacelli <taylormonacelli@gmail.com>
2022-10-19 18:52:01 -0700

    real men dont make git commit comments

--- test2.sh
+++ test2.sh
@@ -8,11 +8,11 @@ docker build -t taylorm/ubuntu .
 docker run --detach --name myubuntu --rm --tty --privileged --volume /sys/fs/cgroup:/sys/fs/cgroup:ro taylorm/ubuntu
 sleep 5
 
-docker exec myubuntu -- bash -c 'apt -qq install'
-docker exec myubuntu -- bash -c 'apt -qy install iputils-ping'
-docker exec myubuntu -- bash -c 'ping -c 10 google.com'
-docker exec myubuntu -- bash -c 'apt -qy install apache2'
-docker exec myubuntu -- bash -c 'systemctl status apache2'
-docker exec myubuntu -- bash -c 'systemctl enable apache2'
-docker exec myubuntu -- bash -c 'systemctl start apache2'
-docker exec myubuntu -- bash -c 'systemctl status apache2'
+docker exec myubuntu bash -c 'apt -qq install'
+docker exec myubuntu bash -c 'apt -qy install iputils-ping'
+docker exec myubuntu bash -c 'ping -c 10 google.com'
+docker exec myubuntu bash -c 'apt -qy install apache2'
+docker exec myubuntu bash -c 'systemctl status apache2'
+docker exec myubuntu bash -c 'systemctl enable apache2'
+docker exec myubuntu bash -c 'systemctl start apache2'
+docker exec myubuntu bash -c 'systemctl status apache2'

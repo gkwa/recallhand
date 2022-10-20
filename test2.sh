54e1d698cf7437981935c82be10b030d53095c60
Taylor Monacelli <taylormonacelli@gmail.com>
2022-10-19 18:45:20 -0700

    real men dont make git commit comments

--- test2.sh
+++ test2.sh
@@ -3,6 +3,8 @@
 git clone https://github.com/robertdebock/docker-ubuntu-systemd.git
 cd docker-ubuntu-systemd
 
+docker build -t taylorm/ubuntu .
+
 docker run --name myubuntu --rm --tty --privileged --volume /sys/fs/cgroup:/sys/fs/cgroup:ro taylorm/ubuntu -d
 sleep 5
 

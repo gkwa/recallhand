bb0b8a5b067ea4b6c857c813846babbb11eddfcc
Taylor Monacelli <taylormonacelli@gmail.com>
2022-10-19 18:54:29 -0700

    real men dont make git commit comments

--- test2.sh
+++ test2.sh
@@ -8,7 +8,7 @@ docker build -t taylorm/ubuntu .
 docker run --detach --name myubuntu --rm --tty --privileged --volume /sys/fs/cgroup:/sys/fs/cgroup:ro taylorm/ubuntu
 sleep 5
 
-docker exec myubuntu bash -c 'apt -qq install'
+docker exec myubuntu bash -c 'apt -qq update'
 docker exec myubuntu bash -c 'apt -qy install iputils-ping'
 docker exec myubuntu bash -c 'ping -c 10 google.com'
 docker exec myubuntu bash -c 'apt -qy install apache2'
